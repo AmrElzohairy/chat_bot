f88c8851b289df7b9a087eeb235e4a54ff49a281
amrElzohairy <aelzohairy9@gmail.com>
2025-05-25 21:35:00 +0300

    refactor in snac bar

--- lib/core/widgets/custom_snac_bar.dart
+++ lib/core/widgets/custom_snac_bar.dart
@@ -1,3 +1,4 @@
+import 'package:chat_bot/core/utils/app_colors.dart';
 import 'package:flutter/material.dart';
 
 customSnackBar(BuildContext context, String message, Color color) {
@@ -9,7 +10,12 @@ customSnackBar(BuildContext context, String message, Color color) {
       duration: const Duration(seconds: 3),
       elevation: 5,
       showCloseIcon: true,
-      content: Text(message),
+      content: Text(
+        message,
+        style: Theme.of(
+          context,
+        ).textTheme.bodyMedium!.copyWith(color: AppColors.white),
+      ),
       backgroundColor: color,
     ),
   );
