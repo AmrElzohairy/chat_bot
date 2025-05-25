ac6d58911feaae206aa8128e70496148fea446eb
amrElzohairy <aelzohairy9@gmail.com>
2025-05-25 15:44:04 +0300

    add images

--- lib/core/utils/app_images.dart
+++ lib/core/utils/app_images.dart
@@ -2,12 +2,36 @@
 class Assets {
   Assets._();
   
-  /// Assets for imagesOnBoardingDarkImage
-  /// assets/images/on_boarding_dark_image.png
-  static const String imagesOnBoardingDarkImage = "assets/images/on_boarding_dark_image.png";
+  /// Assets for imagesAppLogo
+  /// assets/images/app_logo.svg
+  static const String imagesAppLogo = "assets/images/app_logo.svg";
+
+  /// Assets for imagesAppLogoPng
+  /// assets/images/app_logo_png.png
+  static const String imagesAppLogoPng = "assets/images/app_logo_png.png";
+
+  /// Assets for imagesGoogle
+  /// assets/images/google.svg
+  static const String imagesGoogle = "assets/images/google.svg";
+
+  /// Assets for imagesLockClosedOutline
+  /// assets/images/lock-closed-outline.svg
+  static const String imagesLockClosedOutline = "assets/images/lock-closed-outline.svg";
+
+  /// Assets for imagesMailOutline
+  /// assets/images/mail-outline.svg
+  static const String imagesMailOutline = "assets/images/mail-outline.svg";
 
   /// Assets for imagesOnBoardingLightImage
   /// assets/images/on_boarding_light_image.png
   static const String imagesOnBoardingLightImage = "assets/images/on_boarding_light_image.png";
+
+  /// Assets for imagesPersonOutline
+  /// assets/images/person-outline.svg
+  static const String imagesPersonOutline = "assets/images/person-outline.svg";
+
+  /// Assets for imagesUnion
+  /// assets/images/Union.svg
+  static const String imagesUnion = "assets/images/Union.svg";
 }
 
--- lib/features/auth/presentation/cubits/views/sign_in_view.dart
+++ lib/features/auth/presentation/cubits/views/sign_in_view.dart
@@ -1,10 +1,56 @@
+import 'package:chat_bot/core/utils/app_colors.dart';
+import 'package:chat_bot/core/utils/app_images.dart';
+import 'package:chat_bot/core/utils/spacing_widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../../../../../core/helpers/get_current_brightness.dart';
 
 class SignInView extends StatelessWidget {
   const SignInView({super.key});
-static const routeName = '/sign_in_view';
+  static const routeName = '/sign_in_view';
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 12.0),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              VerticalSpace(height: 70),
+              Align(
+                alignment: Alignment.center,
+                child: SvgPicture.asset(Assets.imagesAppLogo),
+              ),
+              VerticalSpace(height: 20),
+              SizedBox(
+                width: 335.w,
+                child: Text(
+                  'Welcome Back!',
+                  textAlign: TextAlign.center,
+                  style: Theme.of(context).textTheme.headlineLarge,
+                ),
+              ),
+              SizedBox(
+                width: 335.w,
+                child: Text(
+                  'Enter your login details',
+                  textAlign: TextAlign.center,
+                  style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                    color:
+                        getCurrentBrightness(context)
+                            ? AppColors.lightSecondaryText
+                            : AppColors.darkSecondaryText,
+                    fontSize: 15.sp,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
