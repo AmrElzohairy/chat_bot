bc7abcbd743ab63ddf7b37c42615319ad2b63c9b
amrElzohairy <aelzohairy9@gmail.com>
2025-05-26 12:57:13 +0300

    add nav bar icons

--- lib/core/utils/app_images.dart
+++ lib/core/utils/app_images.dart
@@ -1,6 +1,10 @@
 // ignore_for_file: prefer_single_quotes
 class Assets {
   Assets._();
+  
+  /// Assets for imagesAndroid12AppLogo
+  /// assets/images/android12_app_logo.png
+  static const String imagesAndroid12AppLogo = "assets/images/android12_app_logo.png";
 
   /// Assets for imagesAppLogo
   /// assets/images/app_logo.svg
@@ -10,14 +14,21 @@ class Assets {
   /// assets/images/app_logo_png.png
   static const String imagesAppLogoPng = "assets/images/app_logo_png.png";
 
+  /// Assets for imagesAppLuncherIcon
+  /// assets/images/app_luncher_icon.png
+  static const String imagesAppLuncherIcon = "assets/images/app_luncher_icon.png";
+
+  /// Assets for imagesChatBotInActive
+  /// assets/images/chat_bot_in_active.svg
+  static const String imagesChatBotInActive = "assets/images/chat_bot_in_active.svg";
+
   /// Assets for imagesGoogle
   /// assets/images/google.svg
   static const String imagesGoogle = "assets/images/google.svg";
 
   /// Assets for imagesLockClosedOutline
   /// assets/images/lock-closed-outline.svg
-  static const String imagesLockClosedOutline =
-      "assets/images/lock-closed-outline.svg";
+  static const String imagesLockClosedOutline = "assets/images/lock-closed-outline.svg";
 
   /// Assets for imagesMailOutline
   /// assets/images/mail-outline.svg
@@ -25,14 +36,22 @@ class Assets {
 
   /// Assets for imagesOnBoardingLightImage
   /// assets/images/on_boarding_light_image.png
-  static const String imagesOnBoardingLightImage =
-      "assets/images/on_boarding_light_image.png";
+  static const String imagesOnBoardingLightImage = "assets/images/on_boarding_light_image.png";
 
   /// Assets for imagesPersonOutline
   /// assets/images/person-outline.svg
   static const String imagesPersonOutline = "assets/images/person-outline.svg";
 
+  /// Assets for imagesProfileIcon
+  /// assets/images/profile_icon.svg
+  static const String imagesProfileIcon = "assets/images/profile_icon.svg";
+
+  /// Assets for imagesSavedIcon
+  /// assets/images/saved_icon.svg
+  static const String imagesSavedIcon = "assets/images/saved_icon.svg";
+
   /// Assets for imagesUnion
   /// assets/images/Union.svg
   static const String imagesUnion = "assets/images/Union.svg";
 }
+
