577b146d22f97cfd472e113820f0e29a3f10a9a7
amrElzohairy <aelzohairy9@gmail.com>
2025-05-26 13:31:18 +0300

    finish nav bar style

--- lib/features/main_views/presentation/views/main_views.dart
+++ lib/features/main_views/presentation/views/main_views.dart
@@ -1,11 +1,94 @@
+import 'package:chat_bot/core/helpers/get_current_brightness.dart';
+import 'package:chat_bot/core/utils/app_colors.dart';
+import 'package:chat_bot/core/utils/app_images.dart';
+import 'package:chat_bot/features/main_views/presentation/views/chat_bot_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
-class MainViews extends StatelessWidget {
+class MainViews extends StatefulWidget {
   const MainViews({super.key});
   static const routeName = '/main_views';
 
   @override
+  State<MainViews> createState() => _MainViewsState();
+}
+
+class _MainViewsState extends State<MainViews> {
+  int _selectedIndex = 0;
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text('Main Views')));
+    return Scaffold(
+      body: IndexedStack(
+        index: _selectedIndex,
+        children: const [
+          ChatBotView(),
+          Center(child: Text('Saved View')),
+          Center(child: Text('Profile View')),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        elevation: 0,
+        backgroundColor:
+            getCurrentBrightness(context) ? AppColors.white : AppColors.black,
+        selectedItemColor: AppColors.primaryColor,
+        currentIndex: _selectedIndex,
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              Assets.imagesChatBotInActive,
+              colorFilter: const ColorFilter.mode(
+                AppColors.grey,
+                BlendMode.srcIn,
+              ),
+            ),
+            activeIcon: SvgPicture.asset(
+              Assets.imagesChatBotInActive,
+              colorFilter: const ColorFilter.mode(
+                AppColors.primaryColor,
+                BlendMode.srcIn,
+              ),
+            ),
+            label: 'Chat Bot',
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(Assets.imagesSavedIcon , colorFilter: const ColorFilter.mode(
+                AppColors.grey,
+                BlendMode.srcIn,
+              ),
+            ),
+            activeIcon: SvgPicture.asset(
+              Assets.imagesSavedIcon,
+              colorFilter: const ColorFilter.mode(
+                AppColors.primaryColor,
+                BlendMode.srcIn,
+              ),
+            ),
+            label: 'Saved',
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(Assets.imagesProfileIcon  , colorFilter: const ColorFilter.mode(
+                AppColors.grey,
+                BlendMode.srcIn,
+              ),
+            ),
+            activeIcon: SvgPicture.asset(
+              Assets.imagesProfileIcon,
+              colorFilter: const ColorFilter.mode(
+                AppColors.primaryColor,
+                BlendMode.srcIn,
+              ),
+            ),
+            label: 'Profile',
+          ),
+        ],
+      ),
+    );
   }
 }
