1d2ae46791a420ee7108e18adf941d549212628f
amrElzohairy <aelzohairy9@gmail.com>
2025-05-27 21:33:27 +0300

    edit keys

--- lib/core/networking/api_keys.dart
+++ lib/core/networking/api_keys.dart
@@ -1,13 +1,7 @@
 class ApiKeys {
-  static const String baserUrl = "https://aichatbot.runasp.net/api/";
-  static const String signIn = "Auth/SignIn";
-  static const String signUp = "Auth/SignUp";
-  static const String refreshToken = "Auth/RefreshToken";
-  static const String featuredCars = "Car/Featured";
-  static const String recommendedCars = "Car/Recommended";
-  static const String toggleFavorite = "Favorite/ToggleFavorite";
-  static const String favoritesMe = "Favorite/Favorites/me";
-  static const String userData = "User/me";
-  static const String carSearch = "Car/Search";
-  static const String carDetails = "Car";
+  static const String baserUrl = "https://aichatbot.runasp.net/api/v1/";
+  static const String signIn = "auth/signIn";
+  static const String signUp = "auth/signUp";
+  static const String refreshToken = "auth/refreshToken";
+  static const String userData = "user/me";
 }
