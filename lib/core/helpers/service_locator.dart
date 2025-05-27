c9a80f53637204ab43b43c29cc25fcf9f33c561a
amrElzohairy <aelzohairy9@gmail.com>
2025-05-27 21:43:18 +0300

    add Main Views Repo to service locator

--- lib/core/helpers/service_locator.dart
+++ lib/core/helpers/service_locator.dart
@@ -1,6 +1,8 @@
 import 'package:chat_bot/core/networking/api_services.dart';
 import 'package:chat_bot/features/auth/data/repo/auth_repo.dart';
 import 'package:chat_bot/features/auth/data/repo/auth_repo_impl.dart';
+import 'package:chat_bot/features/main_views/data/repo/main_vews_repo.dart';
+import 'package:chat_bot/features/main_views/data/repo/main_views_repo_impl.dart';
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 
@@ -9,4 +11,5 @@ final getIt = GetIt.instance;
 void setupServiceLocator() {
   getIt.registerSingleton<ApiService>(ApiService(dio: Dio()));
   getIt.registerSingleton<AuthRepo>(AuthRepoImpl(api: getIt<ApiService>()));
+  getIt.registerSingleton<MainViewsRepo>(MainViewsRepoImpl(api: getIt<ApiService>()));
 }
