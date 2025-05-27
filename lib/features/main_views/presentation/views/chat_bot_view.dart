372467e4d350a59ac210d70c9be8e2526e3ed548
amrElzohairy <aelzohairy9@gmail.com>
2025-05-27 22:02:30 +0300

    add  start Sesson methode

--- lib/features/main_views/presentation/views/chat_bot_view.dart
+++ lib/features/main_views/presentation/views/chat_bot_view.dart
@@ -1,12 +1,29 @@
+import 'dart:developer';
+
 import 'package:chat_bot/core/utils/spacing_widgets.dart';
+import 'package:chat_bot/features/main_views/data/models/start_chat_session_body.dart';
+import 'package:chat_bot/features/main_views/presentation/cubits/cubit/start_session_cubit.dart';
 import 'package:chat_bot/features/main_views/presentation/views/widgets/bot_image_with_suggetions.dart';
 import 'package:chat_bot/features/main_views/presentation/views/widgets/chat_bot_field.dart';
 import 'package:chat_bot/features/main_views/presentation/views/widgets/chat_bubble_sliver_list.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class ChatBotView extends StatelessWidget {
+class ChatBotView extends StatefulWidget {
   const ChatBotView({super.key});
   static const routeName = '/chat_bot_view';
+
+  @override
+  State<ChatBotView> createState() => _ChatBotViewState();
+}
+
+class _ChatBotViewState extends State<ChatBotView> {
+  @override
+  void initState() {
+    _startSesson();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Padding(
@@ -31,5 +48,20 @@ class ChatBotView extends StatelessWidget {
         ],
       ),
     );
+  }
+
+  void _startSesson() {
+    try {
+      context.read<StartSessionCubit>().startChatSession(
+        const StartChatSessionBody(
+          botId: "79492a69-23a6-469a-cd59-08dd9d4bc7bf",
+          userId: "892aa5a5-2de4-4094-a192-b902f61cf17e",
+          sessionTitle: "Chat Bot",
+        ),
+      );
+      log("Chat session started successfully");
+    } on Exception catch (e) {
+      log("Error in ChatBotView in startSesson method : $e");
+    }
   }
 }
