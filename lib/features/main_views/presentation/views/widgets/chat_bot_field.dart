bb6b00c0833c47d327c284e9562f56d0d04ece9c
amrElzohairy <aelzohairy9@gmail.com>
2025-05-28 20:32:43 +0300

    add message controller to texr field

--- lib/features/main_views/presentation/views/widgets/chat_bot_field.dart
+++ lib/features/main_views/presentation/views/widgets/chat_bot_field.dart
@@ -1,14 +1,34 @@
+import 'package:chat_bot/core/cache/cache_constants.dart';
+import 'package:chat_bot/core/cache/cache_helper.dart';
 import 'package:chat_bot/core/helpers/get_current_brightness.dart';
 import 'package:chat_bot/core/utils/app_colors.dart';
 import 'package:chat_bot/core/utils/spacing_widgets.dart';
+import 'package:chat_bot/features/main_views/data/models/chat_body_model.dart';
+import 'package:chat_bot/features/main_views/presentation/cubits/send_message_cubit/send_message_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class ChatBotFiled extends StatelessWidget {
+class ChatBotFiled extends StatefulWidget {
   const ChatBotFiled({super.key});
 
   @override
+  State<ChatBotFiled> createState() => _ChatBotFiledState();
+}
+
+class _ChatBotFiledState extends State<ChatBotFiled> {
+  List<ChatBodyModel> chatBody = [];
+  late TextEditingController _messageController;
+
+  @override
+  void initState() {
+    super.initState();
+    _messageController = TextEditingController();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final sessionId = CacheHelper.getString(key: CacheKeys.sessionId);
     return Row(
       children: [
         Expanded(
@@ -32,6 +52,7 @@ class ChatBotFiled extends StatelessWidget {
               borderRadius: BorderRadius.circular(12.r),
             ),
             child: TextField(
+              controller: _messageController,
               decoration: InputDecoration(
                 fillColor:
                     getCurrentBrightness(context)
@@ -56,7 +77,12 @@ class ChatBotFiled extends StatelessWidget {
           backgroundColor: AppColors.primaryColor,
           radius: 35.r,
           child: IconButton(
-            onPressed: () {},
+            onPressed: () {
+              context.read<SendMessageCubit>().sendMessage(
+                sessionId!,
+                ChatBodyModel(message: _messageController.text.trim()),
+              );
+            },
             icon: Icon(Icons.send, color: AppColors.white, size: 28.sp),
           ),
         ),
